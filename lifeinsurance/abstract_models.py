f8cf4e289b90037c6484407b6fadab302288a609
Manoj Gautam <manojit.gautam@gmail.com>
2018-09-10 07:05:14 +0545

    Project created

---
+++ lifeinsurance/abstract_models.py
@@ -0,0 +1,19 @@
+'''
+This model contains all the abstract models, that used in core application. 
+Database schema aren't created from this schema
+'''
+
+from django.db import models
+
+class Person(models.Model):
+    '''
+    Person abstract class having basic person attributes
+    ''' 
+
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    
+
+    class Meta:
+        abstract = True
